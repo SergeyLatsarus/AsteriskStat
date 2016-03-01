f9c421055c486fb6aa15d3b856bf86386ebe52fe
Sergey Latsarus <lyats1@gmail.com>
2016-03-01 15:33:03 +0300

    refactoring

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ __author__ = 'akr0bat'
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
-managers_num = ('205', '207', '223', '251', '300', '221', '227')
-managers_names = { '205':'Andrey Radostnov', '207':'Dmitriy Aleksentsev', '223':'Alina Kondratieva', '300':'Support', '221':'Alexander Melkumyants', '227':'Mikhail Tsyganov'}
+managers_num = ('205', '207', '223', '251', '300', '221', '227', '206')
+managers_names = { '205':'Andrey Radostnov', '207':'Dmitriy Aleksentsev', '223':'Alina Kondratieva', '300':'Support', '221':'Alexander Melkumyants', '227':'Mikhail Tsyganov', '206':'Sergey Kiselev'}
 
 dealingers_num = ('241', '242', '243')
 dealingers_names = {'241' : 'Dealing - 1', '242' : 'Dealing - 2', '243' : 'Dealing - 3'}
