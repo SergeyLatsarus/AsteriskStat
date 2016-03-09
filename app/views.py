b2ea7e1d14239776d9483180ade5815a0d2967be
Sergey Latsarus <lyats1@gmail.com>
2016-03-09 17:28:35 +0300

    new features

--- app/views.py
+++ app/views.py
@@ -14,7 +14,6 @@ from config import managers_num, managers_names, dealingers_num, work_hours, all
 @app.route('/')
 @app.route('/index', methods = ['GET', 'POST'])
 
-
 def index():
 
     form = UserDate(request.form)
@@ -44,6 +43,37 @@ def index():
         visible_managers = visible_managers,
         managers = all_managers)
 
+
+@app.route('/stat', methods = ['GET', 'POST'])
+
+def stat():
+
+    form = UserDate(request.form)
+    requsted_date_from = form.input_date_from._value()
+    requsted_date_to = form.input_date_to._value()
+
+    all_managers = getData.main(requsted_date_from, requsted_date_to)
+    visible_managers = {}
+    visible_dealingers = {}
+
+    for num in managers_num:
+        if all_managers.has_key(num):
+            visible_managers[num] = all_managers[num]
+
+    for num in dealingers_num:
+        if all_managers.has_key(num):
+            visible_dealingers[num] = all_managers[num]
+
+    return render_template("stat.html",
+        work_hours = work_hours,
+        all_hours = all_hours,
+        today = requsted_date_from,
+        title = 'Get Report',
+        form = form,
+        dealingers = visible_dealingers,
+        managers_names = managers_names,
+        visible_managers = visible_managers,
+        managers = all_managers)
 
 
 @app.route('/config')
